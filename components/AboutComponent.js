3d983e6bbf7a551fecf5bd499c1d35010c8d772f
MeliiZa <melissazandman@icloud.com>
2020-07-15 16:22:20 -0700

    Add animatable components

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,6 +4,8 @@ import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
+
 
 const mapStateToProps = state => {
     return {
@@ -58,24 +60,29 @@ class About extends Component {
         if (this.props.partners.errMess) {
             return (
                 <ScrollView>
-                    <Mission />
-                    <Card
-                        title='Community Partners'>
-                        <Text>{this.props.partners.errMess}</Text>
-                    </Card>
+                    <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+
+                        <Mission />
+                        <Card
+                            title='Community Partners'>
+                            <Text>{this.props.partners.errMess}</Text>
+                        </Card>
+                    </Animatable.View>
                 </ScrollView>
             );
         }
         return (
             <ScrollView>
-                <Mission />
-                <Card title="Community Partners">
-                    <FlatList                                       
-                        data={this.props.partners.partners}                  
-                        renderItem={renderPartner}                  // renderItem function has a built-in item property which expects an object, so we can destructure it in the renderPartner fxn
-                        keyExtractor={item => item.id.toString()}  
-                    />  
-                </Card>
+                 <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Mission />
+                    <Card title="Community Partners">
+                        <FlatList                                       
+                            data={this.props.partners.partners}                  
+                            renderItem={renderPartner}                  // renderItem function has a built-in item property which expects an object, so we can destructure it in the renderPartner fxn
+                            keyExtractor={item => item.id.toString()}  
+                        />  
+                    </Card>
+                </Animatable.View>
             </ScrollView>
         );
     }
