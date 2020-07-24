bd74e41b473c1abaf46b86d1b0008974d9b7d047
MeliiZa <melissazandman@icloud.com>
2020-07-24 11:01:40 -0700

    Enable  local  notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,6 +3,8 @@ import { Text, View, ScrollView, StyleSheet,
     Picker, Switch, Button, Modal } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import  * from 'react-native-animatable';
+import * as Permissions from 'expo-permissions';
+import { Notifications } from 'expo';
 
 class Reservation extends Component {
 
@@ -36,6 +38,27 @@ class Reservation extends Component {
             date: '',
             
         });
+    }
+    async obtainNotificationPermission() {
+        const permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            const permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+            return permission;
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        const permission = await this.obtainNotificationPermission();
+        if (permission.status === 'granted') {
+            Notifications.presentLocalNotificationAsync({
+                title: 'Your Campsite Reservation Search',
+                body: 'Search for ' + date + ' requested'
+            });
+        }
     }
 
     render() {
@@ -107,8 +130,11 @@ class Reservation extends Component {
                                 onPress: ()=> {this.resetForm();}
                             },{
                                 text:'OK',
-                                onPress: ()=> {this.resetForm();}
-                            }])}
+                                                   onPress: () => {
+                        this.presentLocalNotification(this.state.date);
+                        this.resetForm();
+                    }}
+                              ])}
                         title='Search'
                         color='#5637DD'
                         accessibilityLabel='Tap me to search for available campsites to reserve'
